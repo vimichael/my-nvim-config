1ed4e7230be5d796b8b3574f5951e979e4b53552
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-05-13 09:18:28 +0900

    automatic sync from ~/dotfiles/synclife

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,104 +1,113 @@
 return {
-  -- Using lazy.nvim
-  -- {
-  --   "metalelf0/black-metal-theme-neovim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("black-metal").setup({
-  --       theme = "taake",
-  --       code_style = {
-  --         comments = "none",
-  --         conditionals = "none",
-  --         functions = "none",
-  --         keywords = "none",
-  --         headings = "bold", -- Markdown headings
-  --         operators = "none",
-  --         keyword_return = "none",
-  --         strings = "none",
-  --         variables = "none",
-  --       },
-  --     })
-  --     require("black-metal").load()
-  --   end,
-  -- },
-  {
-    "oahlen/iceberg.nvim"
-  },
-  {
-    "sainnhe/gruvbox-material",
-    config = function()
-      vim.g.gruvbox_material_background = 'hard'
-    end
-  },
-  {
-    "Skardyy/makurai-nvim"
-  },
-  {
-    "blazkowolf/gruber-darker.nvim",
-    opts = {
-      bold = false,
-    },
-  },
-  {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    italic = false,
-  },
-  { "ellisonleao/gruvbox.nvim" },
-  {
-    "vague2k/vague.nvim",
-    config = function()
-      require("vague").setup({
-        -- optional configuration here
-        -- transparent = true,
-        style = {
-          -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-          boolean = "none",
-          number = "none",
-          float = "none",
-          error = "none",
-          comments = "none",
-          conditionals = "none",
-          functions = "none",
-          headings = "bold",
-          operators = "none",
-          strings = "none",
-          variables = "none",
+	{
+		"slugbyte/lackluster.nvim",
+	},
+	{
+		"vim-scripts/zenesque.vim",
+	},
+	{
+		"jaredgorski/fogbell.vim",
+	},
+	-- Using lazy.nvim
+	-- {
+	--   "metalelf0/black-metal-theme-neovim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   config = function()
+	--     require("black-metal").setup({
+	--       theme = "taake",
+	--       code_style = {
+	--         comments = "none",
+	--         conditionals = "none",
+	--         functions = "none",
+	--         keywords = "none",
+	--         headings = "bold", -- Markdown headings
+	--         operators = "none",
+	--         keyword_return = "none",
+	--         strings = "none",
+	--         variables = "none",
+	--       },
+	--     })
+	--     require("black-metal").load()
+	--   end,
+	-- },
+	{
+		"oahlen/iceberg.nvim",
+	},
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.g.gruvbox_material_background = "hard"
+		end,
+	},
+	{
+		"Skardyy/makurai-nvim",
+	},
+	{
+		"blazkowolf/gruber-darker.nvim",
+		opts = {
+			bold = false,
+		},
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		italic = false,
+	},
+	{ "ellisonleao/gruvbox.nvim" },
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({
+				-- optional configuration here
+				-- transparent = true,
+				style = {
+					-- "none" is the same thing as default. But "italic" and "bold" are also valid options
+					boolean = "none",
+					number = "none",
+					float = "none",
+					error = "none",
+					comments = "none",
+					conditionals = "none",
+					functions = "none",
+					headings = "bold",
+					operators = "none",
+					strings = "none",
+					variables = "none",
 
-          -- keywords
-          keywords = "none",
-          keyword_return = "none",
-          keywords_loop = "none",
-          keywords_label = "none",
-          keywords_exception = "none",
+					-- keywords
+					keywords = "none",
+					keyword_return = "none",
+					keywords_loop = "none",
+					keywords_label = "none",
+					keywords_exception = "none",
 
-          -- builtin
-          builtin_constants = "none",
-          builtin_functions = "none",
-          builtin_types = "none",
-          builtin_variables = "none",
-        },
-        colors = {
-          -- func = "#bc96b0",
-          -- keyword = "#787bab",
-          -- -- string = "#d4bd98",
-          -- string = "#8a739a",
-          -- -- string = "#f2e6ff",
-          -- -- number = "#f2e6ff",
-          -- -- string = "#d8d5b1",
-          -- number = "#8f729e",
-          -- -- type = "#dcaed7",
-        },
-      })
-    end,
-  },
-  {
-    "jnurmine/Zenburn",
-  },
-  {
-    "RRethy/base16-nvim",
-  },
+					-- builtin
+					builtin_constants = "none",
+					builtin_functions = "none",
+					builtin_types = "none",
+					builtin_variables = "none",
+				},
+				colors = {
+					-- func = "#bc96b0",
+					-- keyword = "#787bab",
+					-- -- string = "#d4bd98",
+					-- string = "#8a739a",
+					-- -- string = "#f2e6ff",
+					-- -- number = "#f2e6ff",
+					-- -- string = "#d8d5b1",
+					-- number = "#8f729e",
+					-- -- type = "#dcaed7",
+				},
+			})
+		end,
+	},
+	{
+		"jnurmine/Zenburn",
+	},
+	{
+		"RRethy/base16-nvim",
+	},
 }
