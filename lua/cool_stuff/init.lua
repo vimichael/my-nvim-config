e49d3358fe4b9985b485c64c6080d63a5e696e4f
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-05-22 10:36:31 +0900

    automatic sync from ~/dotfiles/synclife

--- lua/cool_stuff/init.lua
+++ lua/cool_stuff/init.lua
@@ -4,3 +4,7 @@ require("cool_stuff.nts").setup()
 -- keymaps for nts
 vim.keymap.set("n", "[n", ":Nts<CR>")
 vim.keymap.set("n", "[j", ":NtsJournal<CR>")
+
+vim.api.nvim_create_user_command("Path", function()
+	print(vim.fn.expand("%:p"))
+end, {})
