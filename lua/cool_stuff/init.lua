3739f67f6a26a590ea0ae68fc080a9b680c8928d
vimichael <michaelwilliamspersonal@gmail.com>
2025-06-11 17:02:19 +0900

    automatic sync from ~/dotfiles/synclife

--- lua/cool_stuff/init.lua
+++ lua/cool_stuff/init.lua
@@ -1,10 +1,10 @@
 require("cool_stuff.chelpers").setup()
-require("cool_stuff.nts").setup()
+require("cool_stuff.ntsh").setup()
 
 -- keymaps for nts
 vim.keymap.set("n", "[n", ":Nts<CR>")
 vim.keymap.set("n", "[j", ":NtsJournal<CR>")
 
 vim.api.nvim_create_user_command("Path", function()
-	print(vim.fn.expand("%:p"))
+  print(vim.fn.expand("%:p"))
 end, {})
