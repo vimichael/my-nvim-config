1809212c00ceb8e1ab00026fc7ce35e09b9b5d20
vimichael <michaelwilliamspersonal@gmail.com>
2025-06-25 01:22:45 +0900

    automatic sync from ~/dotfiles/synclife

--- init.lua
+++ init.lua
@@ -104,3 +104,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.conceallevel = 2
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "csv",
+  callback = function()
+    vim.cmd("CsvViewEnable display_mode=border")
+    vim.opt_local.wrap = false
+  end,
+})
