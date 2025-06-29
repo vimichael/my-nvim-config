dbaf4c4e45080c5b255fc4897b341e64bee2c112
vimichael <michaelwilliamspersonal@gmail.com>
2025-06-29 19:08:34 +0900

    automatic sync from ~/dotfiles/synclife

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -1,11 +1,11 @@
-return {}
--- return {
---   "sphamba/smear-cursor.nvim",
---   opts = {
---     enabled = true,
---     smear_between_buffers = false,
---     stiffness = 0.9,
---     trailing_stiffness = 0.8,
---     distance_stop_animating = 0.3,
---   },
--- }
+-- return {}
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    enabled = true,
+    smear_between_buffers = false,
+    stiffness = 0.9,
+    trailing_stiffness = 0.8,
+    distance_stop_animating = 0.3,
+  },
+}
