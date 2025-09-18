83530f711c832f9f81c488f887535769b3793b2c
vimichael <michaelwilliamspersonal@gmail.com>
2025-09-19 04:08:36 +0900

    automatic sync from ~/dotfiles/synclife

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -27,7 +27,7 @@ return {
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
         ["<CR>"] = { "select_and_accept", "fallback" },
-        ["<C-e>"] = { "show", "hide", "fallback" },
+        ["<C-e>"] = { "hide", "fallback" },
       },
 
       appearance = {
@@ -50,7 +50,7 @@ return {
           auto_show_delay_ms = 0,
         },
         menu = {
-          auto_show = false,
+          auto_show = true,
           scrollbar = false,
           draw = {
             columns = {
