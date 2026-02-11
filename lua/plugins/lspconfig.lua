5abd656832b554ca724de1595a0e35f5c659636c
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2026-02-10 22:34:28 -0800

    automatic sync from ~/dotfiles/synclife

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -53,220 +53,220 @@ return {
 			local util = require("lspconfig").util
 			local configs = require("lspconfig.configs")
 
-      lspconfig.cmake.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.fortls.setup({
-        capabilities = capabilities,
-        root_dir = require("lspconfig").util.root_pattern("*.f90"),
-      })
-      lspconfig.purescriptls.setup({
-        capabilities = capabilities,
-        filetypes = { "purescript" },
-        settings = {
-          purescript = {
-            addSpagoSources = true,
-          },
-        },
-        flags = {
-          debounce_text_changes = 150,
-        },
-      })
-      lspconfig.ols.setup({
-        capabilities = capabilities,
-        root_dir = require("lspconfig").util.root_pattern("*.odin"),
-      })
-      lspconfig.ocamllsp.setup({
-        capabilities = capabilities,
-        cmd = { "ocamllsp", "--stdio" },
-        filetypes = { "ocaml", "reason" },
-        root_dir = require("lspconfig").util.root_pattern("*.opam", "esy.json", "package.json"),
-      })
-      if not configs.roc_ls then
-        configs.roc_ls = {
-          default_config = {
-            cmd = { "roc_language_server", "--stdio" },
-            capabilities = capabilities,
-            filetypes = {
-              "roc",
-            },
-            single_file_support = true,
-          },
-        }
-      end
-      lspconfig.roc_ls.setup({
-        capabilities = capabilities,
-      })
-      -- lspconfig.gdscript.setup({
-      --   capabilities = capabilities,
-      --   filetypes = { "gd", "gdscript", "gdscript3" },
-      -- })
-      lspconfig.astro.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.nil_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.sqlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.intelephense.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.texlab.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.zls.setup({
-        capabilities = capabilities,
-        cmd = { "zls" },
-      })
-      lspconfig.hls.setup({
-        capabilities = capabilities,
-        single_file_support = true,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        -- cmd = { "lua_ls" },
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }, -- Recognize 'vim' as a global variable
-            },
-            workspace = {
-              library = {
-                vim.api.nvim_get_runtime_file("", true),
-                "${3rd}/love2d/library"
-              }, -- Include Neovim runtime files
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
-      lspconfig.wgsl_analyzer.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.prismals.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-      -- lspconfig.html.setup({
-      --   capabilities = capabilities,
-      --   single_file_support = true,
-      --   filetypes = {
-      --     "templ",
-      --     "html",
-      --     "php",
-      --     "css",
-      --     "javascriptreact",
-      --     "typescriptreact",
-      --     "javascript",
-      --     "typescript",
-      --     "jsx",
-      --     "tsx",
-      --   },
-      -- })
-      -- lspconfig.htmx.setup({
-      --   capabilities = capabilities,
-      --   single_file_support = true,
-      --   filetypes = { "html", "templ" },
-      -- })
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities,
-        single_file_support = true,
-        filetypes = {
-          "templ",
-          "html",
-          "css",
-          "php",
-          "javascript",
-          "javascriptreact",
-          "typescriptreact",
-          "typescript",
-          "jsx",
-          "tsx",
-        },
-      })
-      -- lspconfig.tailwindcss.setup({
-      -- 	capabilities = capabilities,
-      -- 	filetypes = {
-      -- 		"templ",
-      -- 		"html",
-      -- 		"css",
-      -- 		"javascriptreact",
-      -- 		"typescriptreact",
-      -- 		"javascript",
-      -- 		"typescript",
-      -- 		"jsx",
-      -- 		"tsx",
-      -- 	},
-      -- 	root_dir = require("lspconfig").util.root_pattern(
-      -- 		"tailwind.config.js",
-      -- 		"tailwind.config.cjs",
-      -- 		"tailwind.config.mjs",
-      -- 		"tailwind.config.ts",
-      -- 		"postcss.config.js",
-      -- 		"postcss.config.cjs",
-      -- 		"postcss.config.mjs",
-      -- 		"postcss.config.ts",
-      -- 		"package.json",
-      -- 		"node_modules",
-      -- 		".git"
-      -- 	),
-      -- })
-      lspconfig.templ.setup({
-        capabilities = capabilities,
-        filetypes = { "templ" },
-      })
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.fortls.setup({
+				capabilities = capabilities,
+				root_dir = require("lspconfig").util.root_pattern("*.f90"),
+			})
+			lspconfig.purescriptls.setup({
+				capabilities = capabilities,
+				filetypes = { "purescript" },
+				settings = {
+					purescript = {
+						addSpagoSources = true,
+					},
+				},
+				flags = {
+					debounce_text_changes = 150,
+				},
+			})
+			lspconfig.ols.setup({
+				capabilities = capabilities,
+				root_dir = require("lspconfig").util.root_pattern("*.odin"),
+			})
+			lspconfig.ocamllsp.setup({
+				capabilities = capabilities,
+				cmd = { "ocamllsp", "--stdio" },
+				filetypes = { "ocaml", "reason" },
+				root_dir = require("lspconfig").util.root_pattern("*.opam", "esy.json", "package.json"),
+			})
+			if not configs.roc_ls then
+				configs.roc_ls = {
+					default_config = {
+						cmd = { "roc_language_server", "--stdio" },
+						capabilities = capabilities,
+						filetypes = {
+							"roc",
+						},
+						single_file_support = true,
+					},
+				}
+			end
+			lspconfig.roc_ls.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.gdscript.setup({
+			--   capabilities = capabilities,
+			--   filetypes = { "gd", "gdscript", "gdscript3" },
+			-- })
+			lspconfig.astro.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.nil_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.sqlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.intelephense.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.texlab.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+				cmd = { "zls" },
+			})
+			lspconfig.hls.setup({
+				capabilities = capabilities,
+				single_file_support = true,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				-- cmd = { "lua_ls" },
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" }, -- Recognize 'vim' as a global variable
+						},
+						workspace = {
+							library = {
+								vim.api.nvim_get_runtime_file("", true),
+								"${3rd}/love2d/library",
+							}, -- Include Neovim runtime files
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+			lspconfig.wgsl_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.prismals.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.html.setup({
+			--   capabilities = capabilities,
+			--   single_file_support = true,
+			--   filetypes = {
+			--     "templ",
+			--     "html",
+			--     "php",
+			--     "css",
+			--     "javascriptreact",
+			--     "typescriptreact",
+			--     "javascript",
+			--     "typescript",
+			--     "jsx",
+			--     "tsx",
+			--   },
+			-- })
+			-- lspconfig.htmx.setup({
+			--   capabilities = capabilities,
+			--   single_file_support = true,
+			--   filetypes = { "html", "templ" },
+			-- })
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				single_file_support = true,
+				filetypes = {
+					"templ",
+					"html",
+					"css",
+					"php",
+					"javascript",
+					"javascriptreact",
+					"typescriptreact",
+					"typescript",
+					"jsx",
+					"tsx",
+				},
+			})
+			-- lspconfig.tailwindcss.setup({
+			-- 	capabilities = capabilities,
+			-- 	filetypes = {
+			-- 		"templ",
+			-- 		"html",
+			-- 		"css",
+			-- 		"javascriptreact",
+			-- 		"typescriptreact",
+			-- 		"javascript",
+			-- 		"typescript",
+			-- 		"jsx",
+			-- 		"tsx",
+			-- 	},
+			-- 	root_dir = require("lspconfig").util.root_pattern(
+			-- 		"tailwind.config.js",
+			-- 		"tailwind.config.cjs",
+			-- 		"tailwind.config.mjs",
+			-- 		"tailwind.config.ts",
+			-- 		"postcss.config.js",
+			-- 		"postcss.config.cjs",
+			-- 		"postcss.config.mjs",
+			-- 		"postcss.config.ts",
+			-- 		"package.json",
+			-- 		"node_modules",
+			-- 		".git"
+			-- 	),
+			-- })
+			lspconfig.templ.setup({
+				capabilities = capabilities,
+				filetypes = { "templ" },
+			})
 
-      if not configs.ts_ls then
-        configs.ts_ls = {
-          default_config = {
-            cmd = { "typescript-language-server", "--stdio" },
-            capabilities = capabilities,
-            filetypes = {
-              "javascript",
-              "javascriptreact",
-              "typescript",
-              "typescriptreact",
-              "html",
-            },
-            root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
-            single_file_support = true,
-          },
-        }
-      end
-      lspconfig.ts_ls.setup({
-        -- capabilities = capabilities,
-        -- cmd = { "typescript-language-server", "--stdio" },
-        -- filetypes = {
-        --   "javascript",
-        --   "javascriptreact",
-        --   "typescript",
-        --   "typescriptreact",
-        --   "html",
-        -- },
-        -- root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
-        -- single_file_support = true,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
+			if not configs.ts_ls then
+				configs.ts_ls = {
+					default_config = {
+						cmd = { "typescript-language-server", "--stdio" },
+						capabilities = capabilities,
+						filetypes = {
+							"javascript",
+							"javascriptreact",
+							"typescript",
+							"typescriptreact",
+							"html",
+						},
+						root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
+						single_file_support = true,
+					},
+				}
+			end
+			lspconfig.ts_ls.setup({
+				-- capabilities = capabilities,
+				-- cmd = { "typescript-language-server", "--stdio" },
+				-- filetypes = {
+				--   "javascript",
+				--   "javascriptreact",
+				--   "typescript",
+				--   "typescriptreact",
+				--   "html",
+				-- },
+				-- root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
+				-- single_file_support = true,
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
 			lspconfig.cmake.setup({
 				capabilities = capabilities,
 			})
@@ -525,60 +525,61 @@ return {
 
 			lspconfig.pyright.setup({})
 
-      -- lspconfig.pylsp.setup({
-      --   cmd = { "pylsp" },
-      --   capabilities = capabilities,
-      --   root_dir = function(fname)
-      --     local root_files = {
-      --       'pyproject.toml',
-      --       'setup.py',
-      --       'setup.cfg',
-      --       'requirements.txt',
-      --       'Pipfile',
-      --     }
-      --     return util.root_pattern(unpack(root_files))(fname)
-      --         or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-      --   end,
-      --   single_file_support = true,
-      --   -- settings = {
-      --   --   python = {
-      --   --     pythonPath = get_python_path(),
-      --   --   },
-      --   -- },
-      -- })
+			-- lspconfig.pylsp.setup({
+			--   cmd = { "pylsp" },
+			--   capabilities = capabilities,
+			--   root_dir = function(fname)
+			--     local root_files = {
+			--       'pyproject.toml',
+			--       'setup.py',
+			--       'setup.cfg',
+			--       'requirements.txt',
+			--       'Pipfile',
+			--     }
+			--     return util.root_pattern(unpack(root_files))(fname)
+			--         or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+			--   end,
+			--   single_file_support = true,
+			--   -- settings = {
+			--   --   python = {
+			--   --     pythonPath = get_python_path(),
+			--   --   },
+			--   -- },
+			-- })
 
-      lspconfig.marksman.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gleam.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.nim_langserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.omnisharp.setup({
-        capabilities = capabilities,
-        cmd = { "OmniSharp" },
-      })
-      lspconfig.fennel_ls.setup({
-        capabilities = capabilities,
-        cmd = { "fennel-ls" },
-      })
-      lspconfig.rescriptls.setup({
-        capabilities = capabilities,
-        cmd = { "rescript-language-server", "--stdio" },
-        root_dir = require("lspconfig").util.root_pattern("rescript.json"),
-      })
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gleam.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.nim_langserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				cmd = { "OmniSharp" },
+			})
+			lspconfig.fennel_ls.setup({
+				capabilities = capabilities,
+				cmd = { "fennel-ls" },
+			})
+			lspconfig.rescriptls.setup({
+				capabilities = capabilities,
+				cmd = { "rescript-language-server", "--stdio" },
+				root_dir = require("lspconfig").util.root_pattern("rescript.json"),
+			})
 
-      lspconfig.julials.setup({
-        capabilities = capabilities,
-        -- cmd = { "julia-lsp" },
-        cmd = {
-          "julia",
-          "--project=" .. "~/.julia/environments/lsp/",
-          "--startup-file=no",
-          "--history-file=no",
-          "-e", [[
+			lspconfig.julials.setup({
+				capabilities = capabilities,
+				-- cmd = { "julia-lsp" },
+				cmd = {
+					"julia",
+					"--project=" .. "~/.julia/environments/lsp/",
+					"--startup-file=no",
+					"--history-file=no",
+					"-e",
+					[[
             using Pkg
             Pkg.instantiate()
             using LanguageServer
@@ -604,18 +605,24 @@ return {
                     server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
         server.runlinter = true
             run(server)
-        ]]
-        },
-        on_attach = function(client, bufnr)
-          vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-        end,
-        root_dir = require("lspconfig").util.root_pattern("*.jl"),
-      })
-      lspconfig.c3_lsp.setup({
-        capabilities = capabilities,
-        cmd = { "c3lsp" },
-        root_dir = require("lspconfig").util.root_pattern({ "project.json", "*.c3" }),
-      })
-    end,
-  },
+        ]],
+				},
+				on_attach = function(client, bufnr)
+					vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+				end,
+				root_dir = require("lspconfig").util.root_pattern("*.jl"),
+			})
+			lspconfig.c3_lsp.setup({
+				capabilities = capabilities,
+				cmd = { "c3lsp" },
+				root_dir = require("lspconfig").util.root_pattern({ "project.json", "*.c3" }),
+			})
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.r_languageserver.setup({
+			-- 	capabilities = capabilities,
+			-- })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,69 +1,69 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    {
-      'nvim-treesitter/nvim-treesitter-context',
-      opts = {
-        max_lines = 4,
-        multiline_threshold = 2,
-      },
-    },
-  },
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate',
-  config = function()
-    local ts = require('nvim-treesitter')
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+			opts = {
+				max_lines = 4,
+				multiline_threshold = 2,
+			},
+		},
+	},
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
+	config = function()
+		local ts = require("nvim-treesitter")
 
-    -- Install core parsers at startup
-    ts.install({
-      "typst",
-      "purescript",
-      "nim",
-      "vimdoc",
-      "go",
-      "rust",
-      "c",
-      "lua",
-      "python",
-      "html",
-      "css",
-      "javascript",
-      "typescript",
-      "haskell",
-      "zig",
-      "gleam",
-      "wgsl",
-      "php",
-      "nim",
-      "sql",
-      "markdown",
-      "latex",
-    }, {
-      max_jobs = 8,
-    })
+		-- Install core parsers at startup
+		ts.install({
+			"typst",
+			"purescript",
+			"nim",
+			"vimdoc",
+			"go",
+			"rust",
+			"c",
+			"lua",
+			"python",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"haskell",
+			"zig",
+			"gleam",
+			"wgsl",
+			"php",
+			"nim",
+			"sql",
+			"markdown",
+			"latex",
+		}, {
+			max_jobs = 8,
+		})
 
-    local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+		local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
 
-    -- Auto-install parsers and enable highlighting on FileType
-    vim.api.nvim_create_autocmd('FileType', {
-      group = group,
-      desc = 'Enable treesitter highlighting and indentation',
-      callback = function(event)
-        local lang = vim.treesitter.language.get_lang(event.match) or event.match
-        local buf = event.buf
+		-- Auto-install parsers and enable highlighting on FileType
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			desc = "Enable treesitter highlighting and indentation",
+			callback = function(event)
+				local lang = vim.treesitter.language.get_lang(event.match) or event.match
+				local buf = event.buf
 
-        -- Start highlighting immediately (works if parser exists)
-        pcall(vim.treesitter.start, buf, lang)
+				-- Start highlighting immediately (works if parser exists)
+				pcall(vim.treesitter.start, buf, lang)
 
-        -- Enable treesitter indentation
-        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				-- Enable treesitter indentation
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
-        -- Install missing parsers (async, no-op if already installed)
-        ts.install({ lang })
-      end,
-    })
-  end,
+				-- Install missing parsers (async, no-op if already installed)
+				-- ts.install({ lang })
+			end,
+		})
+	end,
 }
